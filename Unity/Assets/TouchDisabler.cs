4f4f66589b13f661c864ae6fde318de5cb0e0562
Aidan O'Flannagain <aoflannagain@sagosago.com>
2021-06-08 18:16:07 -0400

    Space to disable all touch detection. Added Touchable which uses TouchArea and TouchAreaObserver.

---
+++ Unity/Assets/TouchDisabler.cs
@@ -0,0 +1,13 @@
+﻿namespace UsingSagoTouch {
+
+	using UnityEngine;
+	using SagoTouch;
+
+		public class TouchDisabler : MonoBehaviour { 
+		void Update() {
+			if (Input.GetKeyDown("space")) {
+				TouchDispatcher.Instance.enabled = !TouchDispatcher.Instance.enabled;
+			}
+		}
+	}
+}
---
+++ Unity/Assets/Touchable.cs
@@ -0,0 +1,57 @@
+﻿namespace UsingSagoTouch{
+	using UnityEngine;
+	using SagoTouch;
+	using Touch = SagoTouch.Touch;
+
+	public class Touchable : MonoBehaviour
+	{
+		#region Non-Serialized Fields
+
+		[System.NonSerialized]
+		private TouchArea m_TouchArea;
+
+		[System.NonSerialized]
+		private TouchAreaObserver m_TouchAreaObserver;
+
+		#endregion
+
+		#region Properties
+
+		private TouchArea TouchArea {
+			get { return m_TouchArea = m_TouchArea ?? GetComponent<TouchArea>(); }
+		}
+
+		private TouchAreaObserver TouchAreaObserver {
+			get { return m_TouchAreaObserver = m_TouchAreaObserver ?? GetComponent<TouchAreaObserver>(); }
+		}
+
+		#endregion
+
+		#region MonoBehaviour
+
+		private void OnEnable() {
+			this.TouchAreaObserver.TouchUpDelegate = TouchUp;
+			this.TouchAreaObserver.TouchDownDelegate = TouchDown;
+		}
+
+		private void OnDisable() {
+			this.TouchAreaObserver.TouchUpDelegate = null;
+			this.TouchAreaObserver.TouchDownDelegate = null;
+		}
+
+		#endregion
+
+		#region Methods
+
+		private void TouchUp(TouchArea touchArea, Touch touch) {
+			Debug.Log("Touch Up!", this);
+		}
+		private void TouchDown(TouchArea touchArea, Touch touch) {
+			Debug.Log("Touch Down!", this);
+		}
+
+
+		#endregion
+
+	}
+}
