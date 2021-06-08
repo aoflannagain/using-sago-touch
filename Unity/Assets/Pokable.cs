d0c0241b1313bdf64e44394828e8b0e4c4bcdf46
Aidan O'Flannagain <aoflannagain@sagosago.com>
2021-06-08 14:12:30 -0400

    Create an Observer

---
+++ Unity/Assets/Pokable.cs
@@ -0,0 +1,27 @@
+﻿namespace UsingSagoTouch
+{
+    using UnityEngine;
+    using SagoTouch;
+    using Touch = SagoTouch.Touch;
+
+    public class Pokable : MonoBehaviour, ISingleTouchObserver {
+
+        public bool OnTouchBegan(Touch touch) {
+            return false;
+        }
+
+        public void OnTouchMoved(Touch touch) {
+
+        }
+
+        public void OnTouchEnded(Touch touch) {
+
+        }
+
+        public void OnTouchCancelled(Touch touch) {
+
+        }
+
+    }
+
+}
